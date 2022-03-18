71dc5ff291ab41c61773328d4f37c0250f85fd08
Sharma Ketan <sharmaketann@gmail.com>
2022-03-18 17:56:58 +0530

    production deployment

---
+++ client/src/components/Main/index.jsx
@@ -0,0 +1,13 @@
+import Nav from './Nav'
+import styles from './styles.module.css'
+import Pokemon from './Pokemon'
+const Main = () => {
+  return (
+    <>
+      <Nav />
+      <Pokemon />
+    </>
+  )
+}
+
+export default Main
